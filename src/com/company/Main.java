661ec0512ad43296b3c6d423561918653c28cf82
jordansexton-drajem <jsdrajem@gmail.com>
2023-09-21 09:21:34 -0400

    haha

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Restaurant restaurant1 = new Restaurant();
+        restaurant1.name = "Hotdog's";
+        restaurant1.Name();
+        restaurant1.price = 1;
+        restaurant1.bill();
     }
 }
